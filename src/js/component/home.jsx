aa1ec14ee2e034969fe9e918964528a1ec3a462c
oscq12 <orianasofia86@gmail.com>
2022-08-01 13:20:05 +0000

    Listo sin el fetch :sparkles:

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,78 @@
-import React from "react";
+import React , {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+
+	const [tarea, setTarea]= useState({
+		task:"", 
+		isDone:false
+	})
+
+	const [listaTareas, setListaTareas] = useState([])
+
+	const handleChange = (event) =>{
+		setTarea({...tarea,[event.target.name]:event.target.value})
+	}
+
+	const guardarTarea =(event)=>{
+		if (event.key === "Enter"){
+			if(tarea.task.trim() !== ""){
+				setListaTareas([...listaTareas,tarea])
+				setTarea({
+					task:"",
+					isDone: false
+				})
+			}else{
+				alert("Debes escribir una tarea")
+			}
+		}
+	}
+
+	const borrar =(id)=>{
+		let nuevaLista= listaTareas.filter((item,index)=>{
+			return(id !== index)
+		})
+		setListaTareas(nuevaLista)
+	}
+
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="row">
+				<div className="col">
+					<h1>ToDo</h1>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col">
+					<input 
+						type="text"
+					 	placeholder="que quieres"
+						className="dato"
+						onChange={handleChange}
+						onKeyDown={guardarTarea}
+						value={tarea.task}
+						name="task"/>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col">
+					<ul>
+						{
+							listaTareas.map((item,index)=>{
+								return(
+									<li key={index} onClick={()=>borrar(index)} >{item.task}</li>
+								)
+							})
+						}
+						
+					</ul>
+				</div>
+			</div>
+			<p>tienes {listaTareas.length} tareas por hacer</p>
 		</div>
 	);
 };
